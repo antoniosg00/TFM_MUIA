ace3931c2134fc6acdf53823c4a849fa6ff8fd68
Antonio Segovia Galante <127299087+antoniosg00@users.noreply.github.com>
2024-07-15 20:12:18 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,6 +3,18 @@ import numpy as np
 import pygame
 
 def compute_borders(circuit_path, finish_path):
+    """
+    Computes and modifies the circuit and finish line images for edge detection.
+
+    Parameters:
+    - circuit_path: Path to the circuit image file.
+    - finish_path: Path to the finish line image file.
+
+    Returns:
+    - circuit: Processed circuit image (grayscale).
+    - finish: Processed finish line image (grayscale).
+    - position: Position of the modified box (x, y).
+    """
     # Load the circuit and finish image
     circuit = cv2.imread(circuit_path, cv2.IMREAD_GRAYSCALE)
     finish = cv2.imread(finish_path, cv2.IMREAD_GRAYSCALE)
@@ -20,6 +32,16 @@ def compute_borders(circuit_path, finish_path):
 
 
 def modify_box(matrix):
+    """
+    Modifies a matrix to enhance detection of a rectangular box.
+
+    Parameters:
+    - matrix: Binary image matrix.
+
+    Returns:
+    - matrix: Modified matrix with enhanced rectangular box.
+    - top_left: Coordinates of the top-left corner of the box (y, x).
+    """
     # Find the dimensions of the matrix
     rows = len(matrix)
     cols = len(matrix[0])
@@ -46,11 +68,30 @@ def modify_box(matrix):
 
 
 def scale_image(img, factor):
+    """
+    Scales a Pygame image by a given factor.
+
+    Parameters:
+    - img: Pygame image object to scale.
+    - factor: Scaling factor.
+
+    Returns:
+    - scaled_img: Scaled Pygame image.
+    """
     size = round(img.get_width() * factor), round(img.get_height() * factor)
     return pygame.transform.scale(img, size)
 
 
 def blit_rotate_center(win, image, top_left, angle):
+    """
+    Blits a rotated image onto a Pygame window.
+
+    Parameters:
+    - win: Pygame window surface to draw on.
+    - image: Pygame image object to rotate and blit.
+    - top_left: Top-left coordinates (x, y) of the image.
+    - angle: Rotation angle in degrees.
+    """
     rotated_image = pygame.transform.rotate(image, angle)
     new_rect = rotated_image.get_rect(center=image.get_rect(topleft=top_left).center)
     win.blit(rotated_image, new_rect.topleft)
